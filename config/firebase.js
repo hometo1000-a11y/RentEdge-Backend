91c7e30d252eae181237112e0cd273e97bbde230
manmitra25 <manmitra25@gmail.com>
2026-06-26 20:07:30 +0530

    changed firebase.js config-testing

--- config/firebase.js
+++ config/firebase.js
@@ -1,26 +1,9 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
-const key = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
+const key = process.env.FIREBASE_PRIVATE_KEY;
 
-console.log("Project:", process.env.FIREBASE_PROJECT_ID);
-console.log("Client:", process.env.FIREBASE_CLIENT_EMAIL);
+console.log(JSON.stringify(key.slice(0, 60)));
 
-console.log("Contains literal \\n :", key.includes("\\n"));
-console.log("Contains real newline :", key.includes("\n"));
-
-console.log("First 40 chars:", JSON.stringify(key.substring(0, 40)));
-console.log("Last 40 chars:", JSON.stringify(key.substring(key.length - 40)));
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: key,
-    }),
-  });
-
-  console.log("Firebase initialized successfully");
+for (let i = 0; i < 40; i++) {
+  console.log(i, key.charCodeAt(i), JSON.stringify(key[i]));
 }
-
-module.exports = admin;
